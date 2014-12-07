83f192101623016f19d2fd5bd369e806789c2bf0
Christian Grévisse <christian.grevisse.001@student.uni.lu>
2014-12-07 14:11:41 +0100

    DB Delete: include recently added tables

--- db_delete.py
+++ db_delete.py
@@ -1,7 +1,9 @@
-from app.models import Sentence, Entity, Interaction
+from app.models import *
 from app import db
 
 Sentence.query.delete()
 Entity.query.delete()
 Interaction.query.delete()
+EnsemblHGNCMap.query.delete()
+OntologyAnnotation.query.delete()
 db.session.commit()
